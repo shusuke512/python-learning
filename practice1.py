0823a87a1972b593dfafac6e38c739c4f59ec0f4
shusuke512 <shusuke512@icloud.com>
2025-11-01 14:20:31 +0900

    初期設定2

--- practice1.py
+++ practice1.py
@@ -1,3 +1,8 @@
 print("Hello, GitHub!")
 print("Learning Python step by step!")
 print("バイセクトを作動中")
+import numpy
+import pandas
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+exit()
